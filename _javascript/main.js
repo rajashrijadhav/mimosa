5d635d32d7390765e0f1580193fb5823e4974ef1
rajashrijadhav <rajashrijadhav93@gmail.com>
2020-04-08 11:35:24 +0530

    first commit

---
+++ _javascript/main.js
@@ -0,0 +1,133 @@
+$(document).ready(function () {
+
+    $('.sliding-page').slick({
+        dots: true,
+        arrows: true,
+        prevArrow: '.main-slider-pagenation .button-prev',
+        nextArrow: '.main-slider-pagenation .button-next',
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+    });
+
+
+
+
+});
+
+$(document).ready(function () {
+
+
+    $('.autoplay').each(function () {
+        $(this).slick({
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            nextArrow: '<a href="#" class="button-next"><i class="fas fa-chevron-right"></i></a>',
+            prevArrow: '<a href="#" class="button-prev"><i class="fas fa-chevron-left"></i></a>'
+
+        });
+    });
+
+
+
+});
+
+$(document).ready(function () {
+
+    $('.testi-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+    });
+
+
+
+
+});
+
+$(document).ready(function () {
+
+    $('.blog-slide').slick({
+        slidesToShow: 3,
+
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        nextArrow: '<a href="#" class="button-next"><i class="fas fa-chevron-right"></i></a>',
+        prevArrow: '<a href="#" class="button-prev"><i class="fas fa-chevron-left"></i></a>'
+    });
+
+
+
+
+});
+
+
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+
+
+
+
+window.onscroll = function () {
+    myFunction()
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > 200) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " is-active";
+}
+
+var mybtn = document.getElementsByClassName("tablinks")[0];
+mybtn.click();
+
+
---
+++ _javascript/vendors/jquery-tab-plugin.js
@@ -0,0 +1,97 @@
+/* Utility : Object.create dosen't work all browsers. */
+    if ( typeof Object.create !== 'function' ) {
+            Object.create = function( obj ) {
+                    function F() {};
+                    F.prototype = obj;
+                    return new F();
+            };
+    }
+
+    (function( $, window, document, undefined ) {
+
+            var Tab = {
+                init: function( options, elem ) {
+                    var self = this;
+                    self.elem = elem;
+                    self.$elem = $( elem );
+
+                    /* Extend Options */
+                    self.options = $.extend( {}, $.fn.rtTab.options, options );
+
+                    self.rtTabs();
+                },
+
+                rtTabs: function() {
+                    var self = this,
+                        eachTab = self.$elem.find('li');
+
+                        /* First Tab Active */
+                        eachTab.first().addClass('active');
+
+                        self.rtTabContent( showFirst = 'yes' );
+                        self.rtClick();
+                        
+                },
+                
+                rtClick: function() {
+                    var self = this,
+                        eachTab = self.$elem.find('li'),
+                        tabLink = eachTab.find('a');
+                    
+                    tabLink.on('click', function(e){
+                        /* Prevent */
+                        e.preventDefault();
+                        
+                        /* Remove Active Class From All Tabs */
+                        eachTab.removeClass('active');
+                        
+                        /* Hide All Tab Contents */
+                        self.rtTabContent();
+                        
+                        /* Add Active Class to Current Tab */
+                        $(this).parent().addClass('active');
+                        
+                        /* Show Active Tab Content */
+                        var activeTab = $(this).attr('href');
+                        jQuery(activeTab).removeClass('hide');
+                        
+                        if ( typeof self.options.onComplete === 'function' ) {
+                            self.options.onComplete.apply( self.elem, arguments );
+                        }
+
+                    });
+                },
+
+                rtTabContent: function(showFirst) {
+                    var self = this,
+                        eachTab = self.$elem.find('li'),
+                        tabLink = eachTab.find('a');
+
+                        tabLink.each( function() {
+                            var link = $(this),
+                                tabContent = link.attr('href');
+                                if( showFirst === 'yes' ) {
+                                    if( ! link.parent().hasClass('active') )
+                                        $(tabContent).addClass('hide');
+                                } else {
+                                    $(tabContent).addClass('hide');
+                                }
+                        });
+                }
+            };
+
+            $.fn.rtTab = function( options ) {
+                    return this.each(function() {
+                            var tab = Object.create( Tab );
+                            tab.init( options, this );
+
+                            /* Store Data */
+                            $.data( this, 'rtTab', tab );
+                    });
+            };
+
+            $.fn.rtTab.options = {
+                    onComplete: null
+            };
+
+    })( jQuery, window, document );
